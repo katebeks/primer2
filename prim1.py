b4754ec3fb55368986552a24c4622f596edd05b5
katebeks <klesta1304@gmail.com>
2023-09-01 23:15:30 +0300

    peremennie

--- prim1.py
+++ prim1.py
@@ -1,7 +1,7 @@
-n = int(input("Введите число от 1 до 99 - "))
-if n % 10 == 1:
-    print("Мне " + str(n) + " год ")
-elif n % 10 < 5:
-    print("Мне " + str(n) + " годa ")
+a = float(input("Введите число - "))
+b = float(input("Введите число - "))
+c = float(input("Введите число - "))
+if (a+b > c) and (a+c > b) and (c+b > a):
+    print("Существует треугольник со сторонами " + str(a) + " " + str(b) + " " + str(c))
 else:
-    print("Мне " + str(n) + " лет ")
+    print("Не существует треугольник со сторонами " + str(a) + " " + str(b) + " " + str(c))

6fa4657bf422d4f7cfe1145a038bb6546541d062
katebeks <klesta1304@gmail.com>
2023-09-01 11:07:53 +0300

    peremennie

--- prim1.py
+++ prim1.py
@@ -1,20 +1,4 @@
-a = "privet"
-b = 5
-c = None
-f = 4.5
-k = True
-h = 7
-j = "project"
-o = 3.544
-y = False
-x = None
-print(a)
-print(b)
-print(c)
-print(f)
-print(k)
-print(h)
-print(j)
-print(o)
-print(y)
-print(x)
+str1 = "123"
+print(type(str1))
+str1 = int(str1)
+print(type(str1))

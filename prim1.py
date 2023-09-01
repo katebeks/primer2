4a3f3737b616814a634ecc67025f35eb3d0b8400
katebeks <klesta1304@gmail.com>
2023-09-01 22:53:17 +0300

    peremennie

--- prim1.py
+++ prim1.py
@@ -1,8 +1,7 @@
-f = "Top Repositories"
-print(f[::])
-print(f[::2])
-print(f[1::2])
-print(f[::-1])
-print(f[5::])
-print(f[:5:])
-print(f[-2:-9:-1])
+n = int(input("Введите число от 1 до 99 - "))
+if n % 10 == 1:
+    print("Мне " + str(n) + " год ")
+elif n % 10 < 5:
+    print("Мне " + str(n) + " годa ")
+else:
+    print("Мне " + str(n) + " лет ")

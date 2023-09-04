04b15b70e864f245a2e5a9cf2cdcb0ac48a680db
katebeks <klesta1304@gmail.com>
2023-09-04 20:44:37 +0300

    peremennie

--- prim1.py
+++ prim1.py
@@ -1,7 +1,12 @@
-x = input("Введите число ")
-k = len(x)
-y = ""
-while k > 0:
-    y = y + x[k-1]
-    k -= 1
-print("Результат - ", y)
+x = int(input("Введите число "))
+k = 0
+a1 = 0
+a2 = 1
+a3 = 0
+while k < x:
+    a3 = a1 + a2
+    print(a3)
+    a1 = a2
+    a2 = a3
+    k += 1
+#print("Результат - ", y)

589fcc650c7274784632549ea85310a5b47e253c
katebeks <klesta1304@gmail.com>
2023-09-04 17:29:38 +0300

    peremennie

--- prim1.py
+++ prim1.py
@@ -1,26 +1,8 @@
-try:
-    year1 = int(input("Введите год рождения  1-го человека- "))
-    month1 = int(input("Введите месяц рождения 1-го человека- "))
-    dey1 = int(input("Введите день рождения 1-го человека- "))
-    year2 = int(input("Введите год рождения 2-го человека- "))
-    month2 = int(input("Введите месяц рождения 2-го человека- "))
-    dey2 = int(input("Введите день рождения 2-го человека- "))
-    if year1 < year2:
-        print("Cтарше 1-ый человек")
-    elif year1 == year2:
-        if month1 < month2:
-            print("Cтарше 1-ый человек")
-        elif month1 == month2:
-            if dey1 < dey2:
-                print("Cтарше 1-ый человек")
-            elif dey1 == dey2:
-                print("Они ровестники")
-            else:
-                print("Cтарше 2-oй человек")
-        else:
-            print("Cтарше 2-oй человек")
-    else:
-        print("Cтарше 2-oй человек")
-except ValueError:
-    print("Некорректное значение")
-
+f = "Top Repositories"
+print(f[::])
+print(f[::2])
+print(f[1::2])
+print(f[::-1])
+print(f[5::])
+print(f[:5:])
+print(f[-2:-(len(f)-2):-1])

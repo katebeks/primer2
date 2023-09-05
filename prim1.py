1895990dcefac74ef2d37bb32ebc1d6fd2fc2a6b
katebeks <klesta1304@gmail.com>
2023-09-05 09:09:42 +0300

    peremennie

--- prim1.py
+++ prim1.py
@@ -1,4 +1,10 @@
-a = [1,7,3,3,2,5,6,1,5,8]
-print(a)
-a.sort()
-print("Два наименьших элемента: ", a[0], ' ', a[1])
+list1 = [1,7,3,3,2,5,6,1,5,8,1,5,1,2,3,5,7,8]
+print(list1)
+a = int(input("Введите начало интервала "))
+b = int(input("Введите конец интервала "))
+#list2 = []
+for i in range(len(list1)-1, 0, -1):
+    for j in range(a, (b+1)):
+        if list1[i] == j:
+            list1.remove(list1[i])
+print(list1)

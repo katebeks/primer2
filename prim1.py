076b8b781518da90f2381c2866afcdfd675cebe2
katebeks <klesta1304@gmail.com>
2023-09-05 10:56:33 +0300

    peremennie

--- prim1.py
+++ prim1.py
@@ -1,14 +1,8 @@
-list1 = [1,7,3,3,2,5,6,1,5,8,1,5,1,2,3,5,7,8]
-print(list1)
-k = 0
-a = int(input("Введите начало интервала "))
-b = int(input("Введите конец интервала "))
-#list2 = []
-for i in range(len(list1)-1, -1, -1):
-    k = 2
-    for j in range(a, (b+1)):
-        if list1[i] == j:
-            k = 1
-    if k == 1:
-        list1.pop(i)        #list1.remove(list1[i])
-print(list1)
+f = "TopuRepositories"
+print(f[::])
+print(f[::2])
+print(f[1::2])
+print(f[::-1])
+print(f[5::])
+print(f[:5:])
+print(f[-2:-14:-1])

8548f2a02841497007bc1bc10947d23543a9e18c
katebeks <klesta1304@gmail.com>
2023-09-05 11:18:24 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,8 +1,21 @@
-f = "TopuRepositories"
-print(f[::])
-print(f[::2])
-print(f[1::2])
-print(f[::-1])
-print(f[5::])
-print(f[:5:])
-print(f[-2:-14:-1])
+import random
+list1 = []
+f = 0
+a = int(input("Введите количество строк "))
+b = int(input("Введите количество столбцов "))
+for i in range(a):
+    list2 = []
+    for j in range(b):
+        f = random.randint(-10, 10)
+        list2.append(f)
+    list1.append(list2)
+for i in list1:
+    print(i)
+k = 0
+maxx = 0
+for i in list1:
+    print(i[k])
+    if maxx < i[k]:
+        maxx = i[k]
+    k += 1
+print("Максимальный элемент диоганали матрицы - ", maxx)

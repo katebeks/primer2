20192d63ba28577d5bbe94304a51f03f4dffcf86
katebeks <klesta1304@gmail.com>
2023-09-09 21:48:34 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,11 +1,27 @@
-str1 = "текст слова мама папа слова текст если много мама что текст слова мама бабушка тетя"
-list1 = list(str1.split(" "))
-print(list1)
-list2 = list(set(list1))
-print(list2)
+list1 = [5, "dog cat mam house mam", "cat mam dad milk", "dad mam dog", "cat cat mam cat", "dog cat house mam"]
+str1 = ""
+list2 = []
+for i in list1:
+    print(i)
+for i in range(1, list1[0]+1):
+    if i == list1[0]:
+        str1 = str1 + list1[i]
+    else:
+        str1 = str1 + list1[i] + " "
+list2 = list(str1.split(" "))
+#print(list2)
+list3 = list(set(list2))
+list3.sort()
+print(list3)
 obj1 = dict()
-for i in list2:
-    j = list1.count(i)
+for i in list3:
+    j = list2.count(i)
     obj1[i] = j
+maxx = 0
+keyy = ""
 for key, val in obj1.items():
     print(key, "-", val )
+    if maxx < val:
+        maxx = val
+        keyy = key
+print("Слово, которое встречается чаще всего - ", keyy)

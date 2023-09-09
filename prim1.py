790292491ec0c9be3f360a8bb6bb263a13820116
katebeks <klesta1304@gmail.com>
2023-09-09 20:50:18 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,3 +1,11 @@
-list1 = {"overone", "EPAM", "IBA Group", "Apalon","Flo"}
-list2 = {"overone","Wargaming", "IBA Group", "Andersen", "Apalon","Flo"}
-print(list1&list2)
+str1 = "текст слова мама папа слова текст если много мама что текст слова мама бабушка тетя"
+list1 = list(str1.split(" "))
+print(list1)
+list2 = list(set(list1))
+print(list2)
+obj1 = dict()
+for i in list2:
+    j = list1.count(i)
+    obj1[i] = j
+for key, val in obj1.items():
+    print(key, "-", val )

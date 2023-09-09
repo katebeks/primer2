e55172123b460c2b727b1df56f53bcc8ad451c45
katebeks <klesta1304@gmail.com>
2023-09-09 23:33:33 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,20 +1,9 @@
-f = open("primer.txt", 'r')
-list1 = f.readlines()
-print(list1)
-str1 = ""
-summ = 0
-list3 = []
-for i in range(len(list1)):
-    str1 = list1[i]
-    str1.replace("\n", "")
-    print(str1)
-    list2 = str1.split(" ")
-    summ += int(list2[2])
-    if int(list2[2]) < 3:
-        list3.append(i)
-srb = summ/len(list1)
-print("Средний балл класса - ", srb)
-print("Оценки меньше 3 баллов:")
-for i in list3:
-    print(list1[i])
+f = open("primer.txt", 'a')
+y = 0
+while y!=1:
+    t = input("Введите строку ")
+    if t == "":
+        y = 1
+    else:
+        f.write(t + "\n")
 f.close()

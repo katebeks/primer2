57b0d35668dbd229299f200f32a23f19f95dbf3f
katebeks <klesta1304@gmail.com>
2023-09-09 16:12:24 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,23 +1,3 @@
-import random
-list1 = []
-f = 0
-a = int(input("Введите количество строк "))
-b = int(input("Введите количество столбцов "))
-for i in range(a):
-    list2 = []
-    for j in range(b):
-        f = random.randint(-9, 9)
-        list2.append(f)
-    list1.append(list2)
-for i in list1:
-    print(i)
-k = 0
-rez = 0
-o = 0
-for i in range(1,a):
-    for j in range(k+1):
-        o = list1[i][j]
-        if o < 0:
-            rez += 1
-    k += 1
-print("Кооличество чисел меньше 0 под главной диагональю - ", rez)
+list1 = {"overone", "EPAM", "IBA Group", "Apalon","Flo"}
+list2 = {"overone","Wargaming", "IBA Group", "Andersen", "Apalon","Flo"}
+print(list1&list2)

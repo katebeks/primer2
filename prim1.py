0133ed9f37fb2162883f232ee8d6692b58982388
katebeks <klesta1304@gmail.com>
2023-09-10 17:44:11 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,9 +1,30 @@
+import math
 import random
-ch = int(input("Введите число "))
-def rang(ch):
-    k = 0
-    while ch != 0:
-        ch = ch // 10
-        k +=1
+def krug():
+    k = int(input("Введите радиус круга - "))
+    k = math.pi * k*k
     return (k)
-print("Колличество разрядов числа - ", rang(ch))
+
+def priamoug():
+    a = int(input("Введите сторону 1 прямоугольника - "))
+    b = int(input("Введите сторону 2 прямоугольника - "))
+    k = a * b
+    return (k)
+
+def treug():
+    a = int(input("Введите 1 сторону треугольника "))
+    b = int(input("Введите 2 сторону треугольника "))
+    c = int(input("Введите 3 сторону треугольника "))
+    p = (a + b + c)/2
+    k = math.sqrt(p*(p-a)*(p-b)*(p-c))
+    return (k)
+print("Сделайте выбор:\n1. Найти площадь круга\n2. Найти площадь прямоугольника\n3. Найти площадь треугольника")
+v = int(input())
+if v == 1:
+    print("Площадь круга ", krug())
+elif v == 2:
+    print("Площадь прямоугольника ", priamoug())
+elif v == 3:
+    print("Площадь треугольника ", treug())
+else:
+    print("Такого варианта нет")

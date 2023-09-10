5511d3ca1ec22eb0b756056c16f20ecdaf5650d5
katebeks <klesta1304@gmail.com>
2023-09-10 11:16:58 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,9 +1,11 @@
-f = open("primer.txt", 'a')
-y = 0
-while y!=1:
-    t = input("Введите строку ")
-    if t == "":
-        y = 1
-    else:
-        f.write(t + "\n")
-f.close()
+with (open("primer.txt", 'r', encoding='utf-8') as f):
+    list1 = f.readlines()
+    print(list1)
+    print("Колличество строк - ", len(list1))
+    m = 1
+    for i in list1:
+        print("Символов в ", m, " строке - ", len(i.replace("\n", '')))
+        list2 = list(i.split(" "))
+        print("Слов в ", m, " строке - ", len(list2))
+        m += 1
+

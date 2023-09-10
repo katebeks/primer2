da144e6c92ea91d710a7da93011f464b91a9b60b
katebeks <klesta1304@gmail.com>
2023-09-10 20:17:14 +0300

    matritsia 1

--- prim1.py
+++ prim1.py
@@ -1,30 +1,8 @@
-import math
-import random
-def krug():
-    k = int(input("Введите радиус круга - "))
-    k = math.pi * k*k
-    return (k)
-
-def priamoug():
-    a = int(input("Введите сторону 1 прямоугольника - "))
-    b = int(input("Введите сторону 2 прямоугольника - "))
-    k = a * b
-    return (k)
-
-def treug():
-    a = int(input("Введите 1 сторону треугольника "))
-    b = int(input("Введите 2 сторону треугольника "))
-    c = int(input("Введите 3 сторону треугольника "))
-    p = (a + b + c)/2
-    k = math.sqrt(p*(p-a)*(p-b)*(p-c))
-    return (k)
-print("Сделайте выбор:\n1. Найти площадь круга\n2. Найти площадь прямоугольника\n3. Найти площадь треугольника")
-v = int(input())
-if v == 1:
-    print("Площадь круга ", krug())
-elif v == 2:
-    print("Площадь прямоугольника ", priamoug())
-elif v == 3:
-    print("Площадь треугольника ", treug())
-else:
-    print("Такого варианта нет")
+str1 = "lakjfnv;ha z dhfnv jan;kjdfnv lknlkdfb  alkdfjblka"
+list1 = list(str1)
+print(list1)
+for i in range(len(list1)-1, -1,-1):
+    if list1.count(list1[i]) > 1:
+        list1.pop(i)
+list1 = tuple(list1)
+print(list1)

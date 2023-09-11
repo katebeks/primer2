08d0cedfec6a2267e45805e3836a1fd88d7323cf
katebeks <klesta1304@gmail.com>
2023-09-11 13:14:11 +0300

    dekorator

--- prim1.py
+++ prim1.py
@@ -1,8 +1,13 @@
-x = int(input("Введите число - "))
-def sumcifr(x):
-    if x // 10 == 0:
-        return x
-    else:
-        return x%10+sumcifr(x//10)
-
-print(sumcifr(x))
+def registr1(func):
+    def wrapper(*x):
+        y = func(*x)
+        y = list(y.split(" "))
+        y[1] = str(y[1]).upper()
+        y = str(y[0]) + " " + str(y[1])
+        print(y)
+    return wrapper
+@registr1
+def hello(x):
+     return "Привет "+x
+x = input("Введите имя ")
+hello(x)

1034480163e9dc8e977a392dda09c3cbddcb3815
katebeks <klesta1304@gmail.com>
2023-09-13 23:38:21 +0300

    dekorator

--- prim1.py
+++ prim1.py
@@ -1,16 +1,29 @@
-def registr1(func):
-    def wrapper():
-        y = func()
-        #print(y)
-        #y = list(y.split(" "))
-        y = str(y).upper()
-        #y = str(y[0]) + " " + str(y[1])
-        print("Привет "+y)
-    return wrapper
-@registr1
-def hello():
-    x = input("Введите имя ")
-    print("Привет "+x)
-    return x
+class Animals:
+    def __init__(self, vid, name, age):
+        self.vid = vid
+        self.name = name
+        self.age = age
 
-hello()
+    def vid_a(self):
+        print("Принадлежность: " + self.vid)
+
+    def name_a(self):
+        print("Кличка - " + self.name)
+
+    def age_a(self):
+        print("Возраст - " + str(self.age))
+
+
+
+cat1 = Animals("кот","Барсик", 3)
+cat2 = Animals("кот","Васька", 1)
+dog1 = Animals("собака","Джек", 5)
+cat1.vid_a()
+cat1.name_a()
+cat1.age_a()
+cat2.name_a()
+cat2.vid_a()
+cat2.age_a()
+dog1.name_a()
+dog1.vid_a()
+dog1.age_a()

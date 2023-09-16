e1d8369ac747f230b868d391e0c12758c6d37732
katebeks <klesta1304@gmail.com>
2023-09-16 17:59:08 +0300

    dekorator

--- prim1.py
+++ prim1.py
@@ -13,17 +13,36 @@ class Animals:
     def age_a(self):
         print("Возраст - " + str(self.age))
 
+class Dikie(Animals):
+    def __init__(self, vid, name, age):
+        super().__init__(vid,name,  age)
 
+    def vivod(self):
+        print("Это дикое животное")
+
+
+class Domashnie(Animals):
+    def __init__(self, vid, name, age):
+        super().__init__(vid, name, age)
+
+    def runing(self):
+        print("Это домашнее животное")
+
+class Morskkie(Animals):
+    def __init__(self, vid, name, age):
+        super().__init__(vid, name, age)
+
+    def run(self):
+        print("Это морское животное")
 
 cat1 = Animals("кот","Барсик", 3)
-cat2 = Animals("кот","Васька", 1)
-dog1 = Animals("собака","Джек", 5)
-cat1.vid_a()
-cat1.name_a()
-cat1.age_a()
-cat2.name_a()
-cat2.vid_a()
-cat2.age_a()
-dog1.name_a()
+olen = Dikie("олень","Кузя", 7)
+olen.name_a()
+olen.vivod()
+dog1 = Domashnie("собака","Бобик",4)
 dog1.vid_a()
-dog1.age_a()
+dog1.runing()
+fish1 = Morskkie("минтай","Каспер",20)
+fish1.name_a()
+fish1.run()
+fish1.age_a()

8e953dd599e40cef660fc0afe265c81723cf8e0f
klesta1304@gmail.com <klesta1304@gmail.com>
2023-10-18 16:50:15 +0300

    Vstavka v konfirentia

--- prim1.py
+++ prim1.py
@@ -1,22 +1,23 @@
 import sqlite3
 conn = sqlite3.connect("database1.db")
 currs = conn.cursor()
+#zaprosdb = """CREATE TABLE Konfirentia(Name TEXT, dataprovedenia TEXT, point TEXT, Organozatori TEXT, dniprovedenia INTEGER, Uchastvuut INTEGER, Dokladchiki INTEGER);"""
 zaprosdb = """CREATE TABLE IF NOT EXISTS Konfirentia(Name TEXT, dataprovedenia TEXT, point TEXT, Organozatori TEXT, dniprovedenia INTEGER, Uchastvuut INTEGER, Dokladchiki INTEGER);"""
 conn.execute(zaprosdb)
 conn.commit()
-insert1 = """INSERT INTO Uchaqstie values ("Pavlov Ivan", "13.05.2023", "14.05.2023", "razvitie organizmov", "sintez, razvitie", "30.05.2023", "yes", "5.06.2023")"""
+insert1 = """INSERT INTO Konfirentia values ("Geografii", "01.06.2023", "Miskorena", "BGU", "3", "6", "3")"""
 conn.execute(insert1)
 conn.commit()
-insert1 = """INSERT INTO Uchaqstie values ("Garavin Alexsandr", "13.05.2023", "14.05.2023", "migracii zhivotnih", "pereselenie, migracia, osedlie zhivotnie", "30.05.2023", "yes", "5.06.2023")"""
+insert1 = """INSERT INTO Konfirentia values ("Biologii", "03.06.2023", "Miskorena", "BGPU", "3", "8", "4")"""
 conn.execute(insert1)
 conn.commit()
-insert1 = """INSERT INTO Uchaqstie values ("Petrova Valentina", "13.05.2023", "13.05.2023", "metodika obuchenia biologii", "objee chasnoe metodi", "30.05.2023", "yes", "5.06.2023")"""
+insert1 = """INSERT INTO Konfirentia values ("Himii", "02.06.2023", "Miskorena", "BPGU", "4", "6", "3")"""
 conn.execute(insert1)
 conn.commit()
-insert1 = """INSERT INTO Uchaqstie values ("Pavlov Ivan", "13.05.2023", "13.05.2023", "uslovia razmnozhenia organozmov", "sintez, razmnozhenie", "30.05.2023", "yes", "5.06.2023")"""
+insert1 = """INSERT INTO Konfirentia values ("Ingenernaia", "03.06.2023", "Miskorena", "BNTU", "3", "7", "3")"""
 conn.execute(insert1)
 conn.commit()
-insert1 = """INSERT INTO Uchaqstie values ("Povilina Irina", "13.05.2023", "13.05.2023", "metodika obuchenia biologii", "objee chasnoe metodi", "30.05.2023", "yes", "5.06.2023")"""
+insert1 = """INSERT INTO Konfirentia values ("Ekologii", "01.06.2023", "Miskorena", "BGU", "3", "10", "7")"""
 conn.execute(insert1)
 conn.commit()
 currs.close()

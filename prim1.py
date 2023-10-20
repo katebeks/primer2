9a6188321b0e18690f3f32355058d87a13b0f5e3
klesta1304@gmail.com <klesta1304@gmail.com>
2023-10-20 19:33:58 +0300

    Izmenenie i udalenie

--- prim1.py
+++ prim1.py
@@ -2,7 +2,13 @@ import sqlite3
 conn = sqlite3.connect("database1.db")
 currs = conn.cursor()
 #zaprosdb = """CREATE TABLE Konfirentia(Name TEXT, dataprovedenia TEXT, point TEXT, Organozatori TEXT, dniprovedenia INTEGER, Uchastvuut INTEGER, Dokladchiki INTEGER);"""
-zaprosdb = """SELECT * FROM Uchaqsniki WHERE kafedra='himii-biologii';"""
+zap1 = """UPDATE Uchaqsniki SET city='Gomel' WHERE rabota='BGPU';"""
+currs.execute(zap1)
+conn.commit()
+zap2 = """DELETE FROM Uchaqsniki WHERE rabota='BGU';"""
+currs.execute(zap2)
+conn.commit()
+zaprosdb = """SELECT * FROM Uchaqsniki;"""
 currs.execute(zaprosdb)
 rez = currs.fetchall()
 for i in rez:
